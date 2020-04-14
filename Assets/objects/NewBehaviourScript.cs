1f457f9c01576fe4cf57762c7aee6f10f97c5977
Steve_Zhang <wentaozhang0305@gmail.com>
2020-04-13 18:44:38 -0700

    initial commit

---
+++ Assets/objects/NewBehaviourScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    Vector2 startPos, endPos, direction;
+    float touchTimeStart, touchTimeFinish, timeInterval;
+
+    [Range(0.05F, 1f)]
+    public float throwForse = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began)
+        {
+
+            touchTimeStart = Time.time;
+            startPos = Input.GetTouch(0).position;
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Ended)
+        {
+
+            touchTimeFinish = Time.time;
+            timeInterval = touchTimeFinish - touchTimeStart;
+            endPos = Input.GetTouch(0).position;
+            direction = startPos - endPos;
+            GetComponent<Rigidbody2D>().AddForce(-direction / timeInterval * throwForse);
+        }
+	}
+}
